de56b6d8f460a450fad2bef8d2aaa1732384a34a
Suveen Vundavalli <Suveen.Kumar.Vundavalli@clxcommunications.com>
2021-06-09 13:24:22 +0530

    Added ResourceServer init

---
+++ ResourceServer/src/test/java/com/sinch/ws/api/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinch.ws.api.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
