8226d6aa6c736e4dbb4b4f33bc845cff7151a351
Suveen Vundavalli <Suveen.Kumar.Vundavalli@clxcommunications.com>
2021-06-10 13:17:38 +0530

    Added API Gateway setup

---
+++ AlbumResourceServer/src/main/java/com/sinch/ws/api/albumresourceserver/AlbumResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.sinch.ws.api.albumresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumResourceServerApplication.class, args);
+	}
+
+}
